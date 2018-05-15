f7145a56ff55717476d7ddeaca39609d15868bbf
ariathefirst <ariang@ucdavis.edu>
2018-05-14 19:42:17 -0700

    corrected parsed data format, need to fix one more bug

--- plotData.py
+++ plotData.py
@@ -2,19 +2,39 @@ import serial
 import csv
 import re
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import time
 import pandas as pd
 import numpy as np
 from io import StringIO
+import re
 
  
 portPath = "/dev/tty.usbmodem1411"       # Must match value shown on Arduino IDE
 baud = 9600                     # Must match Arduino baud rate
 timeout = 5                       # Seconds
 filename = "data.csv"
-max_num_readings = 50
+max_num_readings = 10
 num_signals = 1
- 
- 
+
+
+# fig = plt.figure()
+# ax1 = fig.add_subplot(1,1,1)
+
+# def animate(i):
+#     pullData = open("filename","r").read()
+#     dataArray = pullData.split('\n')
+#     xar = []
+#     yar = []
+#     for eachLine in dataArray:
+#         if len(eachLine)>1:
+#             x,y = eachLine.split(',')
+#             xar.append(int(x))
+#             yar.append(int(y))
+#     ax1.clear()
+#     ax1.plot(xar,yar)
+# ani = animation.FuncAnimation(fig, animate, interval=1000)
+# plt.show() 
  
 def create_serial_obj(portPath, baud_rate, tout):
     """
@@ -66,92 +86,58 @@ def clean_serial_data(data):
     clean_data = []
     
     for line in data:
-#        line.decode('ascii') # converts arduino binary strings to normal string
-        print(line)
-#        line_data = re.findall("\d*\.\d*|\d*",line) # Find all digits
-        line_data = re.findall(r'\d+', line.decode('ascii')) 
-        line_data = [float(element) for element in line_data if is_number(element)] # Convert strings to float
-        if len(line_data) >= 2:
+        print('line is \n',line)
+        print("length of line is \n", len(line))
+        line_data = re.findall(r"[-+]?\d*\.\d+|\d+", line.decode('ascii')) # finds all floating point nums and digits
+        print("length of line_data is \n", len(line_data))
+        line_data = list(map(float, line_data)) # Convert strings to float
+        print('line_data is %s\n' % (line_data))
+
+
+        if len(line_data) > 0:
             clean_data.append(line_data)
- 
+    print(clean_data)
     return clean_data           
  
-def save_to_csv(data, filename):
-    """
-    Saves a list of lists (data) to filename
-    """
-    with open(filename, 'wb') as csvfile:
-        csvwrite = csv.writer(csvfile)
-        print(csvfile)
-        csvwrite.writerows(data)
-#    np.savetxt('data', filename, fmt='%s') # .csv is a dataframe
-
- 
-def gen_col_list(num_signals):
-    """
-    Given the number of signals returns
-    a list of columns for the data.
-    E.g. 3 signals returns the list: ['Time','Signal1','Signal2','Signal3']
-    """
-    col_list = ['Time']
-    for i in range(1,num_signals+1):
-        col = 'Signal'+str(i)
-        col_list.append(col)
+# def gen_col_list(num_signals):
+#     """
+#     Given the number of signals returns
+#     a list of columns for the data.
+#     E.g. 3 signals returns the list: ['Time','Signal1','Signal2','Signal3']
+#     """
+#     col_list = ['Time']
+#     for i in range(1,num_signals+1):
+#         col = 'Signal'+str(i)
+#         col_list.append(col)
         
-    return col_list
+#     return col_list
     
-def map_value(x, in_min, in_max, out_min, out_max):
-    return (((x - in_min) * (out_max - out_min))/(in_max - in_min)) + out_min
+# def map_value(x, in_min, in_max, out_min, out_max):
+#     return (((x - in_min) * (out_max - out_min))/(in_max - in_min)) + out_min
  
-    
-def simple_plot(csv_file, columns, headers):
-    plt.clf()
-    plt.close()
-    plt.plotfile(csv_file, columns, names=headers, newfig=True)
-    plt.show()
- 
-def plot_csv(csv_file, cols):
-    # Create Pandas DataFrame from csv data
-    data_frame = pd.read_csv(csv_file)
-    # Set the names of the columns
-    data_frame.columns = cols
-    # Set the first column (Time) as the index 
-    data_frame = data_frame.set_index(cols[0])
-    # Map the voltage values from 0-1023 to 0-5
-    data_frame = data_frame.apply(lambda x: map_value(x,0.,1023,0,5))
-    # Bring back the Time column
-    data_frame = data_frame.reset_index()
-    plt.clf()
-    plt.close()
-    # Plot the data
-    data_frame.plot(x=cols[0],y=cols[1:])
-    plt.show()
-    
-    
+
 print ("Creating serial object...")
 serial_obj = create_serial_obj(portPath, baud, timeout)
  
-print ("Reading serial data...")
-serial_data = read_serial_data(serial_obj)
-print (len(serial_data))
- 
-print ("Cleaning data...")
-clean_data =  clean_serial_data(serial_data)
- 
-print ("Saving to csv...")
-np.savetxt('filename', clean_data, fmt='%s') # .csv is a dataframe
-#save_to_csv(clean_data, filename)
-#save_to_csv(serial_data, filename) # changed to serial_data temp to debug clean_data
+while True:
+    print ("Reading serial data...")
+    serial_data = read_serial_data(serial_obj)
+    # print (len(serial_data))
+     
+    print ("Cleaning data...")
+    clean_data =  clean_serial_data(serial_data)
+         
+    print ("Saving to csv...")
+    np.savetxt('arduinoData.csv', clean_data, delimiter= ",", fmt='%s') # .csv is a dataframe
 
- 
-## need to plot csv in a loop at a time step specified by the end user
-## need to figure out how to pass that time step parameter to this program
+    with open('arduinoData.csv', 'rb') as d:
+        data = np.load(d)
+    heightCol = data['Z']
+    print(heightCol)
 
-print ("Plotting data...")
-# #simple_plot(filename, (0,1,2), ['time (s)', 'voltage1', 'voltage2'])
-# #simple_plot(filename, (0,1), ['time (s)', 'voltage1'])
-#plot_csv(filename, gen_col_list(num_signals))
-csv = csv.reader(filename)
-df = pd.read_csv('filename')
-df.to_csv('filename.csv')
+    # df = pd.read_csv('filename')
+    # df.to_csv('filename.csv')
+
+
+    print ("Plotting data...")
 
