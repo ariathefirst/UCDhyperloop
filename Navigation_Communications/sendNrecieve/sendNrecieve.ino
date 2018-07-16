c7ddc6053e7fc7b2d661aedf757d0fc2d60e2360
Naveed <noroumchian@ucdavis.edu>
2018-07-16 15:17:17 -0700

    can now read two data fields from another controller and send data

--- Navigation_Communications/sendNrecieve/sendNrecieve.ino
+++ Navigation_Communications/sendNrecieve/sendNrecieve.ino
@@ -1,12 +1,16 @@
 #include <CAN.h>
 
+char dist[8];
+
+char vel[8];
+
 void setup() {
 
  Serial.begin(9600);
 
  while (!Serial);  Serial.println("CAN Sender");  // start the CAN bus at 500 kbps
 
- while(!CAN.begin(500E3)) {
+ while(!CAN.begin(250E3)) {
 
    Serial.println("Starting CAN failed!");
 
@@ -14,77 +18,53 @@ void setup() {
 
 }
 
-void canEncoder(int data){
 
- int c = data / 255;
 
- CAN.beginPacket(0x11);
 
- CAN.write(data);
+void sendValue(int id, float data){
 
- CAN.write(c);
+ char y[8];
 
- CAN.endPacket();
-
- }int canDecoder(int c, int data){
-
- int val = c*255 + data;
-
- return val;
-
-}struct message{
-
- int value;
-
- int Time;
-
- }message;void sendValue(int id, float data){
-
- char x[8];
-
- dtostrf(data,8,3,x);
+ dtostrf(data,8,3,y);
 
  CAN.beginPacket(id);
 
- CAN.write(x,8);
+ CAN.write(y,8);
 
  CAN.endPacket();
 
- Serial.print(x);
+ Serial.print(y);
 
  }
 
-char dist[8];
 
-char vel[8];
 
 float recieveValue(){
 
-   char x[8];
+   char y[8];
 
    int i=0;
 
-   int pkt;
-
-   if(CAN.parsePacket()){
-
-     while(CAN.available()){
-
-        if(pkt == 0x21){for(i=0;i<=7;i++){dist[i] = (char)CAN.read();}}
-
-        if(pkt == 0x22){for(i=0;i<=7;i++){vel[i] = (char)CAN.read();}}
-
-      }
-
-   }    
-
-     Serial.print(dist);
-
-     Serial.print("\tvel");
-
-     Serial.println(vel);
-
+ 
+ if(CAN.parsePacket()){
+  if(CAN.packetId() == 0x21){
+   
+   while (CAN.available()) {
+      for(i=0;i<=7;i++){dist[i]  =(char)CAN.read();}
+   }
+   
+  }
  }
+ 
+  if(CAN.parsePacket()){
+    if(CAN.packetId() == 0x22){
+  
+      while (CAN.available()) {
+        for(i=0;i<=7;i++){vel[i]  =(char)CAN.read();}
+      }
+    }
+ }
+}
 
  float data;
 
@@ -102,13 +82,13 @@ void loop() {
 
    Serial.print("Sending packet1 ... ");
 
-   Serial.println(data);
+   Serial.println(data);}
+     
+   
+   Serial.print("vel:");
+   Serial.println(vel);
+   Serial.print("dist:");
+   Serial.print(dist);
 
-   }
 
-
-
- 
-
-delay(500);}
-
+  }
