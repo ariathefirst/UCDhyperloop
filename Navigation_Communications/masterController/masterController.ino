0540108ef79b5ab985c31c9b34a061e022f4959f
Naveed <noroumchian@ucdavis.edu>
2018-07-24 16:36:20 -0700

    changed while back into for loop, added new union to represent data

--- Navigation_Communications/masterController/masterController.ino
+++ Navigation_Communications/masterController/masterController.ino
@@ -1,12 +1,20 @@
 #include <CAN.h>
 
 
+union dataFrame{
+  float fl =0;
+  uint8_t bytes[8];
+  } dataFrame;
 
+union dataFrame dist_p;
+union dataFrame dist_w;
+union dataFrame vel_p;
+union dataFrame vel_w;
 
-float dist_p =0;  //propulsion distance
-float dist_w =0;  //propulsion distance
-float vel_p=0;  //propulsion distance
-float vel_w=0;  //propulsion distance
+//float dist_p =0;  //propulsion distance
+//float dist_w =0;  //propulsion distance
+//float vel_p=0;  //propulsion distance
+//float vel_w=0;  //propulsion distance
 char pnmatic_states[4];
 
 int cmd;
@@ -19,6 +27,7 @@ void setup() {
  while(!CAN.begin(500E3)) {
    Serial.println("Starting CAN failed!");
   }
+  Serial.println("Can OK");
 
 }
 
@@ -40,16 +49,52 @@ void sendValue(int id, float data){
 
  }
 
-
+void reciever(){
+  char x[8];
+  int i;
+  int id;
+  for(i; i < 10; i++){
+      
+      // read the data from the can bus
+      if(CAN.parsePacket()){
+        id = CAN.packetId();
+        while (CAN.available()) {       
+          for(i=0;i<=7;i++){
+            x[i]  =(char) CAN.read();
+           
+            Serial.flush();          
+          }
+        }
+       
+      // sort the data recieved using their IDs            
+        if(id == 0x21){
+     
+          for(i=0;i<4;i++){dist_w.bytes[i] = (byte) x[i];}
+          for(i=0;i<4;i++){vel_w.bytes[i] = (byte) x[i+4];}
+        }
+        if(id == 0x22){
+          for(i=0;i<4;i++){vel_p.bytes[i] = (byte) x[i+4];}
+          for(i=0;i<4;i++){dist_p.bytes[i] = (byte) x[i];}
+        }
+        if(id == 0x41){
+          pnmatic_states[0] = x[0]; // FB LS A
+          pnmatic_states[1] = x[1]; // FB LS B
+          pnmatic_states[2] = x[2]; // EB LS A
+          pnmatic_states[3] = x[3]; // EB LS B
+        }
+      }else{} 
+   }
+  }
+  
 //recieve packets from canbus and save them
-void recieveValue(){
+/*void recieveValue(){
    
    char x[8];
    int i = 0;
    int id;
-   unsigned long t = millis;
-// check for packets for 5 miliseconds 
-   while (millis - t < 5){
+   unsigned long t = millis();
+// check for packets, loop 10 times checking for packets, num is arbitrary 
+   for(i; i < 10; i++){
       
       // read the data from the can bus
       if(CAN.parsePacket()){
@@ -61,17 +106,19 @@ void recieveValue(){
           }
         }
       // sort the data recieved using their IDs            
-      if(id == 0x21){dist_w = atof(x);}
-      if(id == 0x22){vel_w = atof(x);}
-      if(id == 0x33){vel_p = atof(x);}
-      if(id == 0x32){dist_p = atof(x);}
-      if(id == 0x41){pnmatic_states[0] = x[0];} // FB LS A
-      if(id == 0x42){pnmatic_states[1] = x[0];} // FB LS B
-      if(id == 0x43){pnmatic_states[2] = x[0];} // EB LS A
-      if(id == 0x44){pnmatic_states[3] = x[0];} // EB LS B
+        if(id == 0x21){dist_w = atof(x);}
+        if(id == 0x22){vel_w = atof(x);}
+        if(id == 0x33){vel_p = atof(x);}
+        if(id == 0x32){dist_p = atof(x);}
+        if(id == 0x41){
+          pnmatic_states[0] = x[0]; // FB LS A
+          pnmatic_states[1] = x[1]; // FB LS B
+          pnmatic_states[2] = x[2]; // EB LS A
+          pnmatic_states[3] = x[3]; // EB LS B
+        }
       }else{} 
    }
-}
+}*/
 
 
 
@@ -79,8 +126,8 @@ void recieveValue(){
 void loop() {
 
  
-
-  recieveValue();
+reciever();
+  //recieveValue();
 
   if(Serial.available()){
     //the case structure to send commands and data to each system
@@ -133,18 +180,18 @@ void loop() {
 
    }
      
-  Serial.print("FB A B ");
-  Serial.print(pnmatic_states[0]);Serial.print(pnmatic_states[1]);
-  Serial.print(" EB AB"); 
-  Serial.print(pnmatic_states[2]);Serial.print(pnmatic_states[3]);
+//  Serial.print("FB A B ");
+//  Serial.print(pnmatic_states[0]);Serial.print(pnmatic_states[1]);
+//  Serial.print(" EB AB"); 
+//  Serial.print(pnmatic_states[2]);Serial.print(pnmatic_states[3]);
   Serial.print("\tvelw:");
-  Serial.print(vel_w);
+  Serial.print(vel_w.fl);
   Serial.print("\tdistw:");
-  Serial.print(dist_w);
-   Serial.print("\tvelp:");
-   Serial.print(vel_p);
-   Serial.print("\tdistp:");
-   Serial.println(dist_p);
+  Serial.println(dist_w.fl);
+//   Serial.print("\tvelp:");
+//   Serial.print(vel_p.fl);
+//   Serial.print("\tdistp:");
+//   Serial.println(dist_p.fl);
 
 
 
