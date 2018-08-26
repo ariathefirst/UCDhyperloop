cee212d839a95c83319e18cf356910eb5b830bc0
cameronshinn <39427662+cameronshinn@users.noreply.github.com>
2018-08-26 15:32:56 -0700

    defined some more funcions

--- Navigation_Communications/masterController/masterController_v2.ino
+++ Navigation_Communications/masterController/masterController_v2.ino
@@ -1,19 +1,17 @@
 #include <CAN.h>
 #include <stdbool.h> //not sure if boolean data type is already included in CAN or Ardiuno
 
-
-int togglePneumaticsInput(bool frictionBrakeToggle, bool eddyBrakeToggle, int pneumaticsState) {
+int togglePneumaticsInput(bool frictionBrakeToggle, bool eddyBrakeToggle, int pneumaticsState) { //input the current state and pick which brakes to toggle
 
 	bool pneumaticsBool[4];
 	int pneumaticsNextState;
 	int i;
-	int j; //maybe I can combine i and j to save memory
 	int begin = 2; //set 2 as the default
 	int end = 1; //set 1 as the default
 
 	for(i = 0; i <= 3; i++) {
 		 pneumaticsState % 2 == 1 ? pneumaticsBool[i] = true : pneumaticsBool[i] = false;
-		 pneumaticsState = pneumaticsState / 2;
+		 pneumaticsState /= 2;
 	}
 
 	if(frictionBrakeToggle == true) {
@@ -24,7 +22,7 @@ int togglePneumaticsInput(bool frictionBrakeToggle, bool eddyBrakeToggle, int pn
 		end = 3;
 	}
 
-	for(j = begin; j <= end, i++) {
+	for(i = begin; i <= end; i++) {
 		pneumaticsBool[i] == true ? pneumaticsBool[i] = false : pneumaticsBool[i] = true;
 	}
 
@@ -32,25 +30,78 @@ int togglePneumaticsInput(bool frictionBrakeToggle, bool eddyBrakeToggle, int pn
 	return pneumaticsNextState;
 }
 
-void setup() {
+int sendPacketInt(int data, long id) { //used to send integers along the CAN bus
+	int begin;
+	int end;
 	
-	Serial.begin(9600); //open serial port
-	while(!Serial); //halt operation until serial connection opens
+	begin = CAN.beginPacket(id);
+	CAN.write(data);
+	end = CAN.endPacket();
+
+	if(begin != 1 || end != 1) { //verify that data is sent correctly
+    	Serial.print("Failed to send packet: ");
+    	Serial.println(id);
+    	return 0;
+  	} else {
+		return 1;
+	}
+}
+
+int receivePacketInt(long desiredId) { //used to receive integers from the CAN bus 
+	int packetSize = CAN.parsePacket();
+
+	if(packetSize && CAN.packetRtr() != true && CAN.packetId() == desiredId) { //only receive packets if they have nonzero size
+		if(CAN.available() && CAN.peek() != -1) {
+			return CAN.read();
+		} else {
+			return NULL;
+		}
+	}
+}
+
+int sendRtr(long desiredId) { //request data from different subsystems based on address
+	int begin;
+	int end;
+
+	begin = CAN.beginPacket(desiredId, 1, true);
+	end = CAN.endPacket();
+
+	if(begin != 1 || end != 1) { //verify that data is sent correctly
+    	Serial.print("Failed to send remote transmission request: ");
+    	Serial.println(desiredId);
+    	return 0;
+  	} else {
+		return 1;
+	}
+}
+
+int receiveCallback() {
+
+}
+
+void setup() {
+	Serial.begin(9600); //halt operation until serial connection opens
+	while(!Serial);
 
 	if(!CAN.begin(500E3)) { //halt operation if CAN won't start
 		Serial.println("Starting CAN failed!");
 		while(1);
 	}
-
 }
 
 void loop() {
 
 	if(Serial.available() > 0) {
 		serialBuffer = Serial.read();
-
 	}
 
+	switch(dataTransferState) { //data transfer FSM for sequencing data sent along the CAN bus
+		case 1:
 
+		case 2:
 
+		case 3:
+
+		default:
+	}
 }
